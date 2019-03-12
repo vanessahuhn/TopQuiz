184a169e1d9c466d4199df2f9e712127b49d75ad
vanessahuhn <vanessa.huhn@gmail.com>
2019-03-12 20:50:10 +0100

    model QuestionBank ok

--- app/src/main/java/model/QuestionBank.java
+++ app/src/main/java/model/QuestionBank.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class QuestionBank {
@@ -7,5 +8,21 @@ public class QuestionBank {
     private List<Question> mQuestionList;
     private int mNextQuestionIndex;
 
-    
+    public QuestionBank(List<Question> questionList) {
+        //Shuffle the question list before storing it
+        mQuestionList = questionList;
+        Collections.shuffle(mQuestionList);
+        mNextQuestionIndex = 0;
+    }
+
+    public Question getQuestion() {
+        //Loop over the questions and return a new one at each call
+        if (mNextQuestionIndex == mQuestionList.size()){
+            mNextQuestionIndex = 0;
+        }
+
+        return mQuestionList.get(mNextQuestionIndex++);
+    }
+
+
 }
